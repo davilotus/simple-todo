919079968b26f953de1874ecfc1535bcc7cc04ed
davilotus <davi.lotus@gmail.com>
2023-07-25 20:57:18 -0300

    feat: insert date to item todo

--- src/App.jsx
+++ src/App.jsx
@@ -5,59 +5,58 @@ import { DeleteIcon, StarIcon, SunIcon, MoonIcon } from '@chakra-ui/icons'
 
 export function App() {
 
-  const [todoInput, setTodoinput] = useState('')
-  const [todoList, setTodoList] = useState([])
-  const [todoListChecked, setTodoListChecked] = useState([])
+  const todayDate = new Date().toJSON().slice(0, 10)
+  const [toDoInput, setToDoInput] = useState('')
+  const [toDoDate, setToDoDate] = useState(todayDate)
+  const [toDoList, setToDoList] = useState([])
+  const [toDoListChecked, setToDoListChecked] = useState([])
 
   const { colorMode, toggleColorMode } = useColorMode()
 
-  const addItem = item => {
-    item && setTodoList([...todoList, item])
-    setTodoinput('')
+  const addItem = (item, date) => {
+    if(item && date) {
+      setToDoList([...toDoList, {item:item, date:date}])
+      setToDoInput('')
+    }
   }
 
   const removeItem = (item, list) => {
+    if(list === toDoList) {
+      let newTodo = [...toDoList]
+      let exists = toDoList.find(toFind => toFind.item === item)
 
-    if(list === todoList) {
-      let newTodo = [...todoList]
-      let index = todoList.indexOf(item)
-  
-      if (index > -1) { 
-        newTodo.splice(index, 1)
-        setTodoList(newTodo)
+      if (exists) { 
+        newTodo = newTodo.filter(item => item !== exists)
+        setToDoList(newTodo)
       }
     
-    } else if(list === todoListChecked) {
-
-      let newTodo = [...todoListChecked]
-      let index = todoListChecked.indexOf(item)
+    } else if(list === toDoListChecked) {
+      let newTodo = [...toDoListChecked]
+      let exists = toDoListChecked.find(toFind => toFind.item === item)
   
-      if (index > -1) { 
-        newTodo.splice(index, 1)
-        setTodoListChecked(newTodo)
+      if (exists) { 
+        newTodo = newTodo.filter(item => item !== exists)
+        setToDoListChecked(newTodo)
       }
     }
-
   }
 
-  const itemStatus = (item, status, list) => {
-
+  const itemStatus = (item, date, status, list) => {
     if(status) {
       removeItem(item, list)
-      setTodoListChecked([...todoListChecked, item])
+      setToDoListChecked([...toDoListChecked, {item:item, date:date}])
 
     } else {
-      addItem(item)
+      addItem(item, date)
 
-      let newTodo = [...todoListChecked]
-      let index = todoListChecked.indexOf(item)
+      let newTodo = [...toDoListChecked]
+      let exists = toDoListChecked.find(toFind => toFind.item === item)
   
-      if (index > -1) { 
-        newTodo.splice(index, 1)
-        setTodoListChecked(newTodo)
+      if (exists) { 
+        newTodo = newTodo.filter(item => item !== exists)
+        setToDoListChecked(newTodo)
       }
     }
-
   }
 
   return (    
@@ -67,7 +66,6 @@ export function App() {
       align={'center'}
       flexWrap={'wrap'} 
       flexDir={'column'}
-      className="todo-list"
     >
       <Container size={'sm'} colorScheme={'gray'}>
         <Flex justifyContent={'space-between'} marginBottom={'10px'}>
@@ -81,31 +79,55 @@ export function App() {
 
         <form>
           <Flex gap={'5px'} className="addItem">
-            <Input type="text" onChange={e => setTodoinput(e.target.value)} value={todoInput} placeholder={'Insert your item'}/>
-            <Button type="submit" onClick={(e) => {e.preventDefault(); addItem(todoInput)}}>Add item</Button>
+            <Input 
+              type="text"
+              value={toDoInput}
+              placeholder={'Insert your item'}
+              onChange={e => setToDoInput(e.target.value)}
+            />
+            <Input
+              placeholder="Select Date"
+              min={todayDate}
+              value={todayDate}
+              type="date"
+              width={'250px'}
+              onChange={e => setToDoDate(e.target.value)}
+            />
+            <Button 
+              width={'150px'}
+              type="submit"
+              onClick={(e) => {e.preventDefault(); addItem(toDoInput, toDoDate)}}
+            >
+              Add item
+            </Button>
           </Flex>
-         </form>
+        </form>
 
-        {todoList.length ? (
+        {toDoList.length ? (
           <Box marginTop={'20px'}>
-            {todoList.map((todoItem, key) => {
+            {toDoList.map((todoItem, key) => {
               return (
                 <Card key={key} padding={'10px'} marginBottom={'5px'}>
                   <Flex flexWrap={'wrap'} justifyContent={'space-between'}>
-                    <Checkbox 
-                      onChange={ e => itemStatus(todoItem, e.target.checked, todoList) } 
-                      isChecked={false}
-                    >
-                      {todoItem}
-                    </Checkbox>
-                    <Button type="button" onClick={() => removeItem(todoItem, todoList)} _hover={{ background: 'red.500' }}><DeleteIcon /></Button>
+                    <Flex flexDirection={'column'}>
+                      <Checkbox 
+                        onChange={ e => itemStatus(todoItem.item, todoItem.date, e.target.checked, toDoList) } 
+                        isChecked={false}
+                      >
+                        {todoItem.item}
+                      </Checkbox>
+                      <small style={{'padding-left':'23px', 'font-size': '.6875rem'}}>to do until {todoItem.date}</small>
+                    </Flex>
+                    <Button type="button" onClick={() => removeItem(todoItem.item, toDoList)} _hover={{ background: 'red.500' }}>
+                      <DeleteIcon />
+                    </Button>
                   </Flex>
                 </Card>
               )
             })}
           </Box>
         ) : (
-          (todoListChecked.length > todoList.length) ? (
+          (toDoListChecked.length > toDoList.length) ? (
             <Flex gap={'5px'} flexDirection={'column'} alignItems={'center'} padding={'30px 0'}>
               <StarIcon boxSize={8} color={'yellow.400'}/>
               <Text textAlign={'center'}>You completed your entire list!</Text>
@@ -113,21 +135,27 @@ export function App() {
           ) : (null)
         )}
 
-        {todoListChecked.length ? (
+        {toDoListChecked.length ? (
           <>
             <Box marginTop={'20px'} className="items items-checked">
-              <Heading size={'sm'} marginBottom={'5px'}>{todoListChecked.length} Completed items</Heading>
-              {todoListChecked.map((todoItem, key) => {
+              <Heading size={'sm'} marginBottom={'5px'}>{toDoListChecked.length} Completed items</Heading>
+              
+              {toDoListChecked.map((todoItem, key) => {
                 return (
                   <Card key={key} padding={'10px'} marginBottom={'5px'}>
                     <Flex justifyContent={'space-between'}>
-                      <Checkbox 
-                        onChange={ e => itemStatus(todoItem, e.target.checked, todoListChecked) }
-                        isChecked={true}
-                      >
-                        <s>{todoItem}</s>
-                      </Checkbox>
-                      <Button type="button" onClick={() => removeItem(todoItem, todoListChecked)} _hover={{ background: 'red.500' }}><DeleteIcon /></Button>
+                      <Flex flexDirection={'column'}>
+                        <Checkbox 
+                          onChange={ e => itemStatus(todoItem.item, todoItem.date, e.target.checked, toDoListChecked) }
+                          isChecked={true}
+                        >
+                          <s style={{'opacity':'.5'}}><i>{todoItem.item}</i></s>
+                        </Checkbox>
+                      <small style={{'padding-left':'23px', 'opacity':'.5', 'font-size': '.6875rem'}}>to do until {todoItem.date}</small>
+                      </Flex>
+                      <Button type="button" onClick={() => removeItem(todoItem.item, toDoListChecked)} _hover={{ background: 'red.500' }}>
+                        <DeleteIcon />
+                      </Button>
                     </Flex>
                   </Card>
                 )
